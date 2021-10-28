3e8c111308954d829a35e702fa824b552406e2d7
Cong Trung <42043537+congtrung2k1@users.noreply.github.com>
2021-10-29 01:59:01 +0700

    Oct 29, fix PipeIn, add classShape
    Delete some in PipeIn, add user and userShape to classShape

--- core/Process/PipeIn.py
+++ core/Process/PipeIn.py
@@ -21,29 +21,8 @@ ggmap = 0
 
 def processStart(x: int, y: int) -> list:
 
-	userShape = ggmap.getShape(x, y)
 
-	print(userShape)
-
-
-
-
-	return [x, y]
-
-
-def placeShape(x: int, y: int, level : int = 3) -> list:
-	c = n*n - 1
-
-	while 1:
-		import random
-		r = randint(1, c)
-
-		a = r // n
-		b = r % n
-
-		if 0 < x + a < ggmap.maxN && 0 < y + b < ggmap.maxM: break
-
-	 
+	return [x, y]	 
 
 
 
@@ -53,7 +32,7 @@ def placeShape(x: int, y: int, level : int = 3) -> list:
 # Debug only
 if __name__ == "__main__":
 
-	ggmap = Shape()
+	ggmap = Shape(10, 11)
 
 	processStart(10, 11)
 
